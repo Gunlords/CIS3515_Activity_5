1d2a1301e736117881ed5fba74c3e2284299becf
Gunlords <tur68285@temple.edu>
2025-02-06 11:51:00 -0500

    More Views of the Adapter Kind

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -4,9 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
-import android.widget.AdapterView.OnItemSelectedListener
-
-// TODO Replace ArrayAdapter with your implementation of TextSizeAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,28 +13,21 @@ class MainActivity : AppCompatActivity() {
         val spinner = findViewById<Spinner>(R.id.spinner)
         val displayTextView = findViewById<TextView>(R.id.textDisplay)
 
-        /* Step 1: Populate this array */
-        val numberArray = Array(50){i -> (i + 1) * 2}
+        // Step 1: Populate this array with text sizes (increments of 2)
+        val numberArray = List(50) { i -> (i + 1) * 2 }
 
-        with (spinner) {
-            /* Step 2: Create adapter to display items from array in Spinner */
-            adapter = ArrayAdapter(this@MainActivity, android.R.layout.simple_spinner_item, numberArray)
+        with(spinner) {
+            // Step 2: Use custom TextSizeAdapter instead of ArrayAdapter
+            adapter = TextSizeAdapter(this@MainActivity, numberArray)
 
-
-            // Step 3: Change TextView's text size to the number selected in the Spinner */
-            onItemSelectedListener = object : OnItemSelectedListener {
+            // Step 3: Change TextView's text size to the selected number
+            onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
                 override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                    parent?.run {
-                        displayTextView.textSize = getItemAtPosition(position).toString().toFloat()
-                    }
+                    displayTextView.textSize = numberArray[position].toFloat()
                 }
 
-                override fun onNothingSelected(parent: AdapterView<*>?) {
-
-                }
-
+                override fun onNothingSelected(parent: AdapterView<*>?) {}
             }
         }
-
     }
 }
--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -1,25 +1,32 @@
 package edu.temple.inclassuiacvitivity
 
+import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
 
-class TextSizeAdapter : BaseAdapter() {
+class TextSizeAdapter(private val context: Context, private val textSizes: List<Int>) : BaseAdapter() {
 
-    override fun getCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getCount(): Int = textSizes.size
 
-    override fun getItem(position: Int): Any {
-        TODO("Not yet implemented")
-    }
+    override fun getItem(position: Int): Any = textSizes[position]
 
-    override fun getItemId(position: Int): Long {
-        TODO("Not yet implemented")
-    }
+    override fun getItemId(position: Int): Long = position.toLong()
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("Not yet implemented")
+        // Display selected item with a fixed text size of 22sp
+        val textView = (convertView as? TextView) ?: TextView(context)
+        textView.text = textSizes[position].toString()
+        textView.textSize = 22f // Fixed size for selected item
+        return textView
     }
 
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        // Display each item in the dropdown with its respective text size
+        val textView = (convertView as? TextView) ?: TextView(context)
+        textView.text = textSizes[position].toString()
+        textView.textSize = textSizes[position].toFloat() // Varying sizes in dropdown
+        return textView
+    }
 }
